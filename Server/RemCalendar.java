e3270a4e63c78cf1226c60a8a7c23a97d0e2035a
Wave <wavejaz1@hotmail.com>
2017-04-27 21:55:24 -0400

    updateEvent()

--- Server/RemCalendar.java
+++ Server/RemCalendar.java
@@ -13,33 +13,19 @@ import java.util.Scanner;
 import java.util.TreeMap;
 
 public interface RemCalendar extends Remote {
-	String EchoMessage() throws RemoteException;
-
-	public String getUserName() throws RemoteException;
-
-	public void setUserName(String name) throws RemoteException;
-
-	public boolean calendarExist(String userName) throws RemoteException;
-
-	public boolean createCalendar(String userName) throws RemoteException;
-
-	public boolean addEvent(String timeInterval, String eventDescription, String accessControl) throws RemoteException;
-
-	public String viewCalendar(String userName) throws RemoteException;
-
-	public List<String> deleteEvent(int eventNumber) throws RemoteException;
-
-	public boolean createAnotherCalendar(String userName) throws RemoteException;
-
-	public boolean isOwner(String userName, int calendarNumber) throws RemoteException;
-
-	public String viewAllCalendarsHelper(Map<String, List<String>> map) throws RemoteException;
-
-	public String viewAnyCalendar(String userName, int index) throws RemoteException;
-
-	public List<String> modifyEvent(int eventNumber) throws RemoteException;
-
-	public String viewAllCalendars() throws RemoteException;
-
-	// public void updateEvent(List<String> newEvent) throws RemoteException;
+     String EchoMessage() throws RemoteException;
+     public void setUserName(String name) throws RemoteException;
+     public String getUserName() throws RemoteException;
+     public boolean calendarExist(String userName) throws RemoteException;
+     public boolean createCalendar(String userName) throws RemoteException;
+     public boolean addEvent(String timeInterval, String eventDescription, String accessControl)throws RemoteException;
+     public String viewCalendar(String userName)throws RemoteException;
+     public List<String> deleteEvent(int eventNumber) throws RemoteException;
+     public  boolean createAnotherCalendar(String userName)throws RemoteException;
+     public boolean isOwner(String userName, int calendarNumber) throws RemoteException;
+     public String viewAllCalendarsHelper(Map<String,List<String>> map) throws RemoteException;
+     public  String viewAnyCalendar(String userName, int index)throws RemoteException;
+     public List<String>  modifyEvent(int eventNumber)throws RemoteException;
+     public  String viewAllCalendars()throws RemoteException;
+     public void updateEvent(List<String> newEvent, String userName, int eventNumber) throws RemoteException;
 }
